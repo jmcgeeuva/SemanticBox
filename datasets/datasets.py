8a04b27e13651ba6ea691d6962c9eab814096f3e
Jonathan McGee <tkg5kq@virginia.edu>
2025-01-28 16:38:32 -0500

    added original dataset class back

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -312,3 +312,192 @@ class Action_DATASETS(data.Dataset):
 
     def __len__(self):
         return len(self.video_list)
+
+
+
+class Action_DATASETS_orig(data.Dataset):
+    def __init__(self, list_file, labels_file,
+                 num_segments=1, new_length=1,
+                 image_tmpl='img_{:05d}.jpg', transform=None,
+                 random_shift=True, test_mode=False, index_bias=1, 
+                 height=224, width=224, label_box=False, debug=False,
+                 bounding_boxes=False):
+
+        self.list_file = list_file
+        self.num_segments = num_segments
+        self.seg_length = new_length
+        self.image_tmpl = image_tmpl
+        self.transform = transform
+        self.random_shift = random_shift
+        self.test_mode = test_mode
+        self.loop=False
+        self.index_bias = index_bias
+        self.labels_file = labels_file
+        self.height = height
+        self.width = width
+        self.label_box = label_box
+        self.debug = debug
+        self.bounding_boxes = bounding_boxes
+
+        if self.index_bias is None:
+            if self.image_tmpl == "frame{:d}.jpg":
+                self.index_bias = 0
+            else:
+                self.index_bias = 1
+        self._parse_list()
+        self.initialized = False
+
+    def _load_image(self, directory, idx):
+
+        return [Image.open(os.path.join(directory, self.image_tmpl.format(idx))).convert('RGB')]
+    
+    def _load_teacher_box(self, annotation, idx):
+        box = []
+        if not self.label_box:
+            box = torch.tensor(annotation['frames'][str(idx)]['teacher_box'])
+        else:
+            box = torch.tensor(annotation['frames'][str(idx)]['label_box'])
+
+        return box
+
+    def create_masks(self, mask_indices, height, width, channels=3):
+        if width < self.model_resolution:
+            toksX = 1
+        else:
+           toksX = width // self.model_resolution 
+
+        if height < self.model_resolution:
+            toksY = 1
+        else:
+            toksY = height // self.model_resolution
+        mask = self.set_mask_from_bb(mask_indices, height, width, channels=channels)
+
+        sideX, sideY = toksX * self.model_resolution, toksY * self.model_resolution
+        if sideX == 0:
+            sideX, sideY = self.model_resolution, self.model_resolution
+        # Resize masks to model resolution
+        mask = torch.nn.functional.interpolate(mask, (sideY, sideX))
+        mask = torch.round(mask)
+
+        return mask
+    
+    @property
+    def total_length(self):
+        return self.num_segments * self.seg_length
+    
+    @property
+    def classes(self):
+        classes_all = pd.read_csv(self.labels_file)
+        return classes_all.values.tolist()
+    
+    def _parse_list(self):
+        self.video_list = [VideoRecord(x.strip().split(' ')) for x in open(self.list_file)]
+
+    def _sample_indices(self, record):
+        if record.num_frames <= self.total_length:
+            if self.loop:
+                return np.mod(np.arange(
+                    self.total_length) + randint(record.num_frames // 2),
+                    record.num_frames) + self.index_bias
+            offsets = np.concatenate((
+                np.arange(record.num_frames),
+                randint(record.num_frames, size=self.total_length - record.num_frames)))
+            return np.sort(offsets) + self.index_bias
+        offsets = list()
+        ticks = [i * record.num_frames // self.num_segments
+                 for i in range(self.num_segments + 1)]
+
+        for i in range(self.num_segments):
+            tick_len = ticks[i + 1] - ticks[i]
+            tick = ticks[i]
+            if tick_len >= self.seg_length:
+                tick += randint(tick_len - self.seg_length + 1)
+            offsets.extend([j for j in range(tick, tick + self.seg_length)])
+        return np.array(offsets) + self.index_bias
+
+    def _get_val_indices(self, record):
+        if self.num_segments == 1:
+            return np.array([record.num_frames //2], dtype=int) + self.index_bias
+        
+        if record.num_frames <= self.total_length:
+            if self.loop:
+                return np.mod(np.arange(self.total_length), record.num_frames) + self.index_bias
+            return np.array([i * record.num_frames // self.total_length
+                             for i in range(self.total_length)], dtype=int) + self.index_bias
+        offset = (record.num_frames / self.num_segments - self.seg_length) / 2.0
+        return np.array([i * record.num_frames / self.num_segments + offset + j
+                         for i in range(self.num_segments)
+                         for j in range(self.seg_length)], dtype=int) + self.index_bias
+
+    def __getitem__(self, index):
+        record = self.video_list[index]
+        segment_indices = self._sample_indices(record) if self.random_shift else self._get_val_indices(record)
+        return self.get(record, segment_indices)
+
+    def __call__(self, img_group):
+        return [self.worker(img) for img in img_group]
+
+    def adjust_bb(self, boxes, target_width, target_height):
+        # Compute the center of each bounding box
+        x_center = (boxes[:, 0] + boxes[:, 2]) / 2
+        y_center = (boxes[:, 1] + boxes[:, 3]) / 2
+
+        # Compute half-width and half-height, handling odd dimensions
+        half_width_left = np.floor(target_width / 2)
+        half_width_right = np.ceil(target_width / 2)
+        half_height_top = np.floor(target_height / 2)
+        half_height_bottom = np.ceil(target_height / 2)
+
+        # Create new bounding box coordinates
+        new_x0 = x_center - half_width_left
+        new_y0 = y_center - half_height_top
+        new_x1 = x_center + half_width_right
+        new_y1 = y_center + half_height_bottom
+
+        # Stack the new coordinates into the final tensor
+        new_boxes = torch.stack([new_x0, new_y0, new_x1, new_y1], dim=1)
+        return new_boxes, new_y1-new_y0, new_x1-new_x0
+
+
+    def get(self, record, indices):
+        images = list()
+        bbs = list()
+        
+        with open(os.path.join(record.path, 'annotation.json')) as f:
+            annotation = json.load(f, object_pairs_hook=OrderedDict)
+
+        for i, seg_ind in enumerate(indices):
+            p = int(seg_ind)
+            try:
+                seg_imgs = self._load_image(record.path, p)
+                bb = self._load_teacher_box(annotation, p)
+            except OSError:
+                print('ERROR: Could not read image "{}"'.format(record.path))
+                print('invalid indices: {}'.format(indices))
+                raise
+            images.extend(seg_imgs)
+            bbs.append(bb)
+
+        if self.bounding_boxes:
+            cropped_images = []
+            for i, (x0, y0, x1, y1) in enumerate(bbs):
+                cropped_images.append(
+                    images[i].crop((
+                        int(np.floor(x0)), 
+                        int(np.ceil (y0)), 
+                        int(np.floor(x1)), 
+                        int(np.ceil (y1))
+                    ))
+                )
+
+            process_data = self.transform(cropped_images)
+        else: 
+            process_data = self.transform(images)
+
+        if self.debug:
+            return process_data, record.label, (images, bbs)
+        else:
+            return process_data, record.label
+
+    def __len__(self):
+        return len(self.video_list)
